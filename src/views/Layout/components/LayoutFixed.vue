a1d4906e8616baec197e680b1e62323a8fbc7a98
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-11 00:39:15 -0400

    Level one category - Vue Router configuration

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -17,7 +17,7 @@
           <RouterLink to="/">首页</RouterLink>
         </li>
         <li v-for="item in categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
 
