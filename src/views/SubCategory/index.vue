81857f19e7d8336509285806c82fa8956c95f7bb
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-12 00:54:33 -0400

    Level two category - implemented breadcrumb navigation

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,6 +1,16 @@
 <script setup>
+import { getCategoryFilterAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 
+const categoryData = ref({})
+const route = useRoute()
+async function getCategoryData() {
+  const res = await getCategoryFilterAPI(route.params.id)
+  categoryData.value = res.result
+}
 
+onMounted(() => getCategoryData())
 </script>
 
 <template>
@@ -9,9 +19,10 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: `/category/${ categoryData.parentId }` }">
+          {{ categoryData.parentName }}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
