aebd78c7421b01fd352d453e09508669072e832a
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-12 01:08:17 -0400

    Level two category - basis goodsList render

--- src/apis/category.js
+++ src/apis/category.js
@@ -14,12 +14,29 @@ export function getCategoryAPI(id) {
  * @param {*} id 分类id 
  * @return {*}
  */
-
 export const getCategoryFilterAPI = (id) => {
   return httpInstance({
     url: '/category/sub/filter',
     params: {
       id
     }
+  })
+}
+
+/**
+ * @description: 获取导航数据
+ * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+  return httpInstance({
+    url: '/category/goods/temporary',
+    method: 'POST',
+    data
   })
 }
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category'
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
 const categoryData = ref({})
 const route = useRoute()
@@ -11,6 +12,20 @@ async function getCategoryData() {
 }
 
 onMounted(() => getCategoryData())
+
+const goodsList = ref([])
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+async function getGoodsList() {
+  const res = await getSubCategoryAPI(reqData.value)
+  goodsList.value = res.result.items
+}
+
+onMounted(() => getGoodsList())
 </script>
 
 <template>
@@ -32,7 +47,7 @@ onMounted(() => getCategoryData())
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
-         <!-- 商品列表-->
+         <GoodsItem v-for="good in goodsList" :good="good" :key="good.id"></GoodsItem>
       </div>
     </div>
   </div>
