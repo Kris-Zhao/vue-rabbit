54bb21500c20aa4940baefa3089cdca6196742d9
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-12 01:52:35 -0400

    Level two category - optimized scroll experience when switching routes

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,12 @@ const router = createRouter({
       path: '/login',
       component: Login
     }
-  ]
+  ],
+  scrollBehavior() {
+    return {
+      top: 0
+    }
+  }
 })
 
 export default router
