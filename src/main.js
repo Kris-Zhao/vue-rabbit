d5a37cef4bd309e5d63cec27a3c89b885f64a0f0
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-17 19:06:38 -0400

    Login - Pinia state persistence

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -9,8 +10,10 @@ import { imgLazyDirectivePlugin } from './directives'
 import { componentPlugin } from './components'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(imgLazyDirectivePlugin)
 app.use(componentPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -14,4 +14,6 @@ export const useUserStore = defineStore('user', () => {
     userInfo,
     getUserInfo
   }
+}, {
+  persist: true
 })
