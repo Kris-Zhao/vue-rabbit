266612af635e1c858f9cfc87b7e9b967ea359e0e
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-17 19:16:15 -0400

    Login - use token to achieve multiple templates adaptation for LayoutNav componenet

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore()
 
 </script>
 
@@ -6,8 +9,8 @@
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
