07bd3bd854e2ab70fba38db62a2f6ac4ba04872c
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-17 16:06:06 -0400

    Login - all form validation rules executed/uniform validation

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -28,6 +28,14 @@ const rules = {
   ]
 }
 
+const formRef = ref(null)
+const doLogin = () => {
+  formRef.value.validate((valid) => {
+    if(valid) {
+      // TODO Login
+    }
+  })
+}
 </script>
 
 <template>
@@ -51,8 +59,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
-              status-icon>
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
               </el-form-item>
@@ -64,7 +71,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
