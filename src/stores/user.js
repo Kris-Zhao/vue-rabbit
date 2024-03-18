29ee4278aa7cc087e999aef70d8895057333a847
Yuhang Zhao <zhaoyuhang0830@gmail.com>
2024-03-17 23:22:46 -0400

    Login - signout feature done

--- src/stores/user.js
+++ src/stores/user.js
@@ -10,9 +10,14 @@ export const useUserStore = defineStore('user', () => {
     userInfo.value = res.result
   }
 
+  const clearUserInfo = () => {
+    userInfo.value = {}
+  }
+
   return {
     userInfo,
-    getUserInfo
+    getUserInfo,
+    clearUserInfo
   }
 }, {
   persist: true
